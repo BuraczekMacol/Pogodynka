c7dfdc571833c62dae717e810605d5a9cfc0b74c
BuraczekMacol <macolemail@gmail.com>
2018-07-06 13:34:46 +0200

    Jeszcze nic

---
+++ Pogodynka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pogodynka
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Pogodynka pogodynkuje!");
+        }
+    }
+}
